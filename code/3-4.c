9602a698b538b40bb9ba957018b8053894124867
bin286433-afk <bin286433@gmail.com>
2026-03-20 10:04:36 +0900

    Update 3-4.c

---
+++ code/3-4.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int max_of(int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    int max = 0;
+    for(int i=0; i<count; i++){
+        int num = va_arg(ap, int);
+        if(max<num){
+            max = num;
+        }
+    }
+    va_end(ap);
+    return max;
+
+
+}
+
+
+int main(void){
+    printf("%d\n", max_of(4, 5, 12, 3, 9));
+    printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+    printf("%d\n", max_of(3, 100, 200, 300));
+    return 0;
+}
