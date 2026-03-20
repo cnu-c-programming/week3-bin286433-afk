9da0836a66e81929868241a14c756a8155f07655
bin286433-afk <bin286433@gmail.com>
2026-03-20 09:44:22 +0900

    Update 2-3.c

---
+++ code/2-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void divide(int a, int b, int *q, int *r){
+    int m = a/b;
+    int n = a%b;
+    *q = m;
+    *r = n;
+
+}
+
+
+
+int main(){
+    int a = 27;
+    int b = 4;
+    int q = 0;
+    int r = 0;
+    divide(a, b, &q, &r);
+
+    printf("%d\n%d\n", q, r);
+    return 0;
+}
