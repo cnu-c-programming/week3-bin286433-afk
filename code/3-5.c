6ab285c40aaaec9c6049a1be2b14504c3024163a
bin286433-afk <bin286433@gmail.com>
2026-03-20 10:17:26 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void my_sum(char typ, int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    if(typ == 'S'){
+        for(int i=0; i<count; i++){
+            printf("%s ", va_arg(ap, char*));
+        }
+    }
+    else if(typ == 'C'){
+        for(int i=0; i<count; i++){
+            printf("%c", va_arg(ap, char*));
+        }
+    }
+    else if(typ == 'D'){
+        int sum =0; 
+        for(int i=0; i<count; i++){
+           sum += va_arg(ap,int);
+        }
+        printf("%d", sum);
+    }
+    va_end(ap);
+    return ;
+
+
+}
+
+
+int main(void){
+    my_sum('S', 2, "Hello", "World");
+    printf("\n");
+    my_sum('C', 3, 'C', 'N', 'U');
+    printf("\n");
+    my_sum('D', 4, 10, 20, 30, 40);
+    return 0;
+}
