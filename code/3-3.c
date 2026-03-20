14126991d3ea923a730832e513d79955b5c34038
bin286433-afk <bin286433@gmail.com>
2026-03-20 09:58:09 +0900

    Update 3-3.c

---
+++ code/3-3.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int average(int count, ...){
+    va_list ap;
+    va_start(ap, count);
+    int total = 0;
+    for(int i=0; i<count; i++){
+        total += va_arg(ap, int);
+    }
+    va_end(ap);
+    return total/count;
+
+
+}
+
+
+int main(void){
+    printf("%d\n", average(3, 10, 20, 30));
+    printf("%d\n", average(5, 10, 20, 30, 40, 50));
+    return 0;
+}
